eb50d2515db41f0a8b299fc4c93ecc211a025749
Filip Nedovic <Filip_Nedovic@epam.com>
2023-03-13 16:10:05 +0100

    added import API to the path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://7ipeux91t0.execute-api.us-east-1.amazonaws.com/dev",
   order: "https://7ipeux91t0.execute-api.us-east-1.amazonaws.com/dev",
-  import: "https://7ipeux91t0.execute-api.us-east-1.amazonaws.com/dev",
+  import: "https://ef0yzz2zi3.execute-api.us-east-1.amazonaws.com/dev",
   bff: "https://7ipeux91t0.execute-api.us-east-1.amazonaws.com/dev",
   cart: "https://7ipeux91t0.execute-api.us-east-1.amazonaws.com/dev",
 };
